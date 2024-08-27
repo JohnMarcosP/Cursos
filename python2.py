e7e4e0c510dc9228eea4b1d08541f61d6306f12e
João Marcos <79798116+JohnMarcosP@users.noreply.github.com>
2024-08-27 16:37:21 -0300

    Create python2.py
    Uso de parâmetros

---
+++ python2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+#print(sys.argv[1])
+
+primeiro_numero = int(sys.argv[1])
+segundo_numero = int(sys.argv[2])
+print("Soma = ", primeiro_numero+segundo_numero)
+print("Div = " , primeiro_numero/segundo_numero)
