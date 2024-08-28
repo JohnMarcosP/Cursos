a308aaf38ce94922dc78ffa5cb148860d8fa4f70
João Marcos <79798116+JohnMarcosP@users.noreply.github.com>
2024-08-28 19:42:10 -0300

    Create tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,8 @@
+tuplas_cores = {"amarelo", "azul", "roxo"}
+lista_cores = ["amarelo", "azul", "roxo"]
+#lista_cores[1] = "branco"
+#tuplas_cores[1] = "branco"
+for cor in tuplas_cores:
+  print(cor)
+#print(tuplas_cores)
+#print(lista_cores)
