a4600bc67e81531ce9b1965d394a1eb4b2c12ce5
João Marcos <79798116+JohnMarcosP@users.noreply.github.com>
2024-08-28 19:39:39 -0300

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,10 @@
+nome = "João"
+idade = 18
+minha_lista = ["azul", "amarelo", "verde", "roxo]
+  for letra in nome:
+      print(letra)
+      print("ola)
+#LISTA - OK
+#TUPLAS
+#DICIONARIO
+#CONJUNTOS
